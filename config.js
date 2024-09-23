d411716b0e69d5f86566ac56ede4ec366e4333fb
alfacc22 <alfccount79@gmail.com>
2024-09-24 00:15:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "FHknADCC#TyBtX9oaXxCeI_K20F9r-VBEfv_buWQu4mR1JtghLbA",
+MONGODB: process.env.MONGODB || "mongodb+srv://hasamg:xlwPPyGX6GedR9xD@cluster0.9qiarq4.mongodb.net/",//enter mongo db url
 };
